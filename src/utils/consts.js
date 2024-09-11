ba61e3d3942595ca52c8cbceee6c52284abbffbc
Sojo506 <fsojodev@gmail.com>
2024-09-10 21:01:31 -0600

    Adds consts variables

---
+++ src/utils/consts.js
@@ -0,0 +1,8 @@
+const PORT = process.env.PORT;
+const PG_HOST = process.env.PG_HOST;
+const PG_USER = process.env.PG_USER;
+const PG_PASSWORD = process.env.PG_PASSWORD;
+const PG_NAME = process.env.PG_NAME;
+const PG_PORT = process.env.PG_PORT;
+
+export { PORT, PG_HOST, PG_USER, PG_PASSWORD, PG_NAME, PG_PORT };
